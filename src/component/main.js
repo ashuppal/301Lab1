fad28f046f993ad36bf53da7476693d277990fb2
Ash Uppal <ashuppal@gmail.com>
2022-11-29 17:22:40 -0800

    updated components with states and bootStrap functionality

--- src/component/main.js
+++ src/component/main.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import HornedBeast from './hornedBeast'
-import unicornimage from '../images/unicorn.jpeg'
-
-
+import beastArray from '../data.json'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class Main extends React.Component {
   render () {
     return (
-      <>
-        <HornedBeast
-          title='UniWhal : '
-          description='A unicorn and a narwhal nuzzling their horns'
-          imgUrl = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-          //img={unicornimage}
-
-        />
-
-        <HornedBeast
-          title='Rhino Family : '
-          description='Parent rhino with two babies'
-          imgUrl = "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-        />
-      </>
+      <Container >
+        <Row lg={5} xs={2} sm={3} md={4}>
+          {beastArray.map(beast => (
+            <Col key={beast._id}>
+              <HornedBeast
+                
+                title={beast.title}
+                imgUrl={beast.image_url}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     )
   }
 }
 
 export default Main
+
