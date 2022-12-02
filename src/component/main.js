9e0f35f2fb1b3c3ee38950d335e0fe9a99382e49
Ash Uppal <ashuppal@gmail.com>
2022-12-01 17:44:20 -0800

    added form to fileter the data

--- src/component/main.js
+++ src/component/main.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import HornedBeast from './hornedBeast'
-// import beastArray from '../data.json'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import React from 'react';
+import HornedBeast from './hornedBeast';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import beastData from '../data.json';
 
 class Main extends React.Component {
+
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     beastData : beastData,
+  //     filterData : beastData,
+
+  //   }
+  // }
+  
   render () {
     return (
       <Container >
+      <Form.Label> See animals by horns</Form.Label>
+      <Form.Select onChange = {this.props.filteredHorns}>
+      <option value = "">show every beast</option>
+      <option value = "1">1 horned beast</option>
+      <option value = "2">2 horned beast</option>
+      <option value = "3">3 horned beast</option>
+      <option value = "100">100 horned beast</option>
+      </Form.Select>
         <Row lg={5} xs={2} sm={3} md={4}>
           {this.props.beastArray.map(beast => (
             <Col key={beast._id}>
@@ -20,6 +39,7 @@ class Main extends React.Component {
             </Col>
           ))}
         </Row>
+    
       </Container>
     )
   }
